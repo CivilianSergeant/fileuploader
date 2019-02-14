8ec404bae14bc5c5808618a81c2c0b0878c06b26
himel <himel>
2019-02-14 10:49:47 +0600

    'some-update'

--- src/Helper/HtmlHelper.php
+++ src/Helper/HtmlHelper.php
@@ -4,10 +4,47 @@ namespace Muhimel\Helper;
 class HtmlHelper
 {
     private static $options;
+    private static $assetPath;
+    private static $assets = array();
 
     // Get Asset Path
-    private static function getAssetPath($assetPath){
-        return '../src/assets/'.$assetPath;
+    private static function getAssetPath($fileName)
+    {
+        if(empty(self::$assetPath)){
+            return '../src/assets/'.$fileName;
+        }
+
+        return self::$assetPath.$fileName;
+        
+    }
+
+    public static function setAssetPath($assetPath)
+    {
+        self::$assetPath=$assetPath;
+        return true;
+    }
+
+    public static function setAsset($asset)
+    {
+        array_push(self::$assets,$asset);
+        
+    }
+
+    public static function assets()
+    {
+        
+        if(!empty(self::$assets)){
+            foreach(self::$assets as $asset){
+                $type = substr($asset,strrpos($asset,'.')+1);
+                if($type == 'css'){
+                    self::style($asset);
+                }
+                if($type == 'js'){
+                    self::script($asset);
+                }
+                
+            }
+        }
     }
 
     public static function style($styleFileName)
--- tests/test.php
+++ tests/test.php
@@ -1,5 +1,11 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 use Muhimel\Uploader;
+use Muhimel\Helper\HtmlHelper;
 
+HtmlHelper::setAssetPath('../src/assets/');
+HtmlHelper::setAsset('css/bootstrap.min.css');
+HtmlHelper::setAsset('css/custom.css');
+HtmlHelper::setAsset('js/vue/vue.js');
+HtmlHelper::setAsset('js/axios/axios.min.js');
 Uploader::getUI();
